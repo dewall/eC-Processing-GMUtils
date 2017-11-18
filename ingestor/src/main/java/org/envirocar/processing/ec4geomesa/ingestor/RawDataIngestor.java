0533988501242e959743bae88216d7fc6417a8d0
dewall <a.dewall@52north.org>
2017-11-18 18:19:06 +0100

    added core and ingestor module

---
+++ ingestor/src/main/java/org/envirocar/processing/ec4geomesa/ingestor/RawDataIngestor.java
@@ -0,0 +1,12 @@
+package org.envirocar.processing.ec4geomesa.ingestor;
+
+/**
+ *
+ * @author dewall
+ */
+public class RawDataIngestor {
+
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
