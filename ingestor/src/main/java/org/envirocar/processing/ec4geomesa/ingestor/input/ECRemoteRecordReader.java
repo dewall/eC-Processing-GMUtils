59b5aa462d7137ae338d93e35910ac1793b099c0
dewall <a.dewall@52north.org>
2017-11-22 22:13:30 +0100

    remote record reader

--- ingestor/src/main/java/org/envirocar/processing/ec4geomesa/ingestor/input/ECRemoteRecordReader.java
+++ ingestor/src/main/java/org/envirocar/processing/ec4geomesa/ingestor/input/ECRemoteRecordReader.java
@@ -1,6 +1,12 @@
 package org.envirocar.processing.ec4geomesa.ingestor.input;
 
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import com.squareup.okhttp.ResponseBody;
 import java.io.IOException;
+import java.util.logging.Logger;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.InputSplit;
@@ -11,38 +17,63 @@ import org.apache.hadoop.mapreduce.TaskAttemptContext;
  *
  * @author dewall
  */
-public class ECRemoteRecordReader extends RecordReader<LongWritable, Text>{
+public class ECRemoteRecordReader extends RecordReader<LongWritable, Text> {
+
+    private static final Logger LOG = Logger.getLogger(
+            ECRemoteRecordReader.class.getName());
+
+    private final Text currentValue = new Text();
+    private final OkHttpClient client = new OkHttpClient();
+
+    private boolean processed = false;
+    private TextInputSplit textInput;
+    private Configuration config;
 
     @Override
     public void initialize(InputSplit is, TaskAttemptContext tac) throws IOException, InterruptedException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.textInput = (TextInputSplit) is;
+        this.config = tac.getConfiguration();
     }
 
     @Override
     public boolean nextKeyValue() throws IOException, InterruptedException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (!processed) {
+            String enviroCarURL = textInput.getText();
+
+            Request request = new Request.Builder()
+                    .url(enviroCarURL)
+                    .build();
+
+            Response response = client.newCall(request).execute();
+            ResponseBody body = response.body();
+
+            this.currentValue.clear();
+            this.currentValue.set(body.toString());
+
+            processed = true;
+            return true;
+        }
+        return false;
     }
 
     @Override
     public LongWritable getCurrentKey() throws IOException, InterruptedException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return new LongWritable();
     }
 
     @Override
     public Text getCurrentValue() throws IOException, InterruptedException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return currentValue;
     }
 
     @Override
     public float getProgress() throws IOException, InterruptedException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return processed ? 1.0f : 0.0f;
     }
 
     @Override
     public void close() throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        // not required.
     }
-
-
 
 }
