4f08525e480b19eed29268d1990c54d1e7479946
dewall <a.dewall@52north.org>
2017-11-30 21:36:11 +0100

    added processor type

---
+++ mapmatching/src/main/java/org/envirocar/processing/ec4geomesa/mapmatching/SegmentStatisticsProcessor.java
@@ -0,0 +1,12 @@
+package org.envirocar.processing.ec4geomesa.mapmatching;
+
+/**
+ *
+ * @author dewall
+ */
+public class SegmentStatisticsProcessor {
+    
+    public static void main(String[] args){
+        
+    }
+}
