f7f671778a4cc7cd6b0cb4a7e84fcf08e0a67ca1
dewall <a.dewall@52north.org>
2017-12-01 17:17:07 +0100

    designed AbstractFeatureStore more generic

--- core/src/main/java/org/envirocar/processing/ec4geomesa/core/feature/AbstractFeatureStore.java
+++ core/src/main/java/org/envirocar/processing/ec4geomesa/core/feature/AbstractFeatureStore.java
@@ -1,14 +1,23 @@
 package org.envirocar.processing.ec4geomesa.core.feature;
 
 import com.google.common.base.Joiner;
+import com.google.inject.Inject;
+import java.io.IOException;
 import java.util.List;
 import org.apache.log4j.Logger;
 import org.geotools.data.DataStore;
 import org.geotools.data.DataUtilities;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.factory.CommonFactoryFinder;
 import org.geotools.feature.SchemaException;
 import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.filter.text.cql2.CQL;
+import org.geotools.filter.text.cql2.CQLException;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.filter.Filter;
+import org.opengis.filter.FilterFactory2;
 
 /**
  *
@@ -16,9 +25,13 @@ import org.opengis.feature.simple.SimpleFeatureType;
  */
 public abstract class AbstractFeatureStore<T> {
 
-    private static final Logger LOG = Logger.getLogger(AbstractFeatureStore.class);
+    private static final Logger LOGGER = Logger.getLogger(AbstractFeatureStore.class);
+
+    @Inject
+    protected DataStore dataStore;
 
     protected String tableName;
+    protected String primaryKey;
 
     protected SimpleFeatureType featureType;
     protected SimpleFeatureBuilder featureBuilder;
@@ -27,41 +40,75 @@ public abstract class AbstractFeatureStore<T> {
      * Constructor.
      *
      * @param tableName
-     */
-    public AbstractFeatureStore(String tableName) {
-        this.tableName = tableName;
-        this.featureType = this.createSimpleFeatureType();
-        this.featureBuilder = new SimpleFeatureBuilder(this.featureType);
-    }
-
-    /**
-     * Constructor.
-     *
-     * @param tableName
+     * @param primaryKey
      * @param schema
      * @param dataStore
      */
-    public AbstractFeatureStore(String tableName, List<String> schema, DataStore dataStore) {
+    public AbstractFeatureStore(String tableName, String primaryKey, List<String> schema, DataStore dataStore) {
+        this.tableName = tableName;
+        this.primaryKey = primaryKey;
 
+        try {
+            this.featureType = DataUtilities.createType(tableName, Joiner.on(",").join(schema));
+            this.featureBuilder = new SimpleFeatureBuilder(this.featureType);
+        } catch (SchemaException ex) {
+            LOGGER.error(String.format("Error while creating FeatureType for Table %s", tableName), ex);
+        }
     }
-
-    protected SimpleFeatureType createSimpleFeatureType(
-            List<String> featureAttributes) throws SchemaException {
-        String spec = Joiner.on(",").join(featureAttributes);
-        return DataUtilities.createType(getTableName(), spec);
-    }
-
-    protected abstract SimpleFeatureType createSimpleFeatureType();
-
-    public abstract SimpleFeature createSimpleFeature(T t);
-
-    public abstract T getById(DataStore ds, String id);
 
     public String getTableName() {
         return this.tableName;
     }
 
-    public SimpleFeatureType getFeatureType() {
-        return featureType;
+    public SimpleFeatureType getSimpleFeatureType() {
+        return this.featureType;
     }
+
+    public T getByFeatureID(String id) {
+        try {
+            FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2();
+            Filter filter = ff.id(ff.featureId(id));
+            SimpleFeature feature = fetchSingle(filter);
+
+            if (feature != null) {
+                return createEntityFromFeature(feature);
+            }
+        } catch (IOException ex) {
+            LOGGER.error(String.format("Error while fetching feature for featureId=%s", id), ex);
+        }
+        return null;
+    }
+
+    public T getByID(String id) {
+        try {
+            Filter filter = CQL.toFilter(String.format("%s = %s", this.primaryKey, id));
+            SimpleFeature feature = fetchSingle(filter);
+
+            if (feature != null) {
+                return createEntityFromFeature(feature);
+            }
+        } catch (CQLException ex) {
+            LOGGER.error("Error while creating Filter.", ex);
+        } catch (IOException ex) {
+            LOGGER.error(String.format("Error while fetching feature for %s=%s", primaryKey, id), ex);
+        }
+        return null;
+    }
+
+    private SimpleFeature fetchSingle(Filter filter) throws IOException {
+        SimpleFeatureCollection features = fetch(filter);
+        return !features.isEmpty() ? features.features().next() : null;
+    }
+
+    private SimpleFeatureCollection fetch(Filter filter) throws IOException {
+        SimpleFeatureSource featureSource = this.dataStore.getFeatureSource(tableName);
+        return featureSource.getFeatures(filter);
+    }
+
+    protected abstract SimpleFeatureType createSimpleFeatureType();
+
+    protected abstract T createEntityFromFeature(SimpleFeature sf);
+
+    protected abstract SimpleFeature createSimpleFeature(T t);
+
 }
