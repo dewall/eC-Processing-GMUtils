850149f710e4df3992230149ba0e1f6b276c9c21
dewall <a.dewall@52north.org>
2017-12-02 16:28:05 +0100

    added minor guice properties test

---
+++ core/src/test/java/org/envirocar/processing/ec4geomesa/GuiceTest.java
@@ -0,0 +1,33 @@
+package org.envirocar.processing.ec4geomesa;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.name.Names;
+import java.util.Map;
+import org.envirocar.processing.ec4geomesa.core.GeoMesaConfig;
+import org.envirocar.processing.ec4geomesa.core.GeoMesaDataStoreModule;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author dewall
+ */
+public class GuiceTest {
+
+    private Injector injector;
+
+    @Before
+    public void before() {
+        this.injector = Guice.createInjector(new GeoMesaDataStoreModule());
+    }
+
+    @Test
+    public void testDataStoreProperties() {
+        Map<String, String> datastoreConfig = injector.getInstance(
+                Key.get(Map.class, Names.named(GeoMesaConfig.GEOMESACONFIG)));
+        assertTrue(!datastoreConfig.isEmpty());
+    }
+}
