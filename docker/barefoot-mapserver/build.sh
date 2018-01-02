759f7576afb0d5c23199f5822ae9e7915b51dc9b
dewall <a.dewall@52north.org>
2018-01-02 10:29:03 +0100

    input parameters for configuration

--- docker/barefoot-mapserver/build.sh
+++ docker/barefoot-mapserver/build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-osm_dl_link="http://download.geofabrik.de/europe/germany/nordrhein-westfalen-latest.osm.pbf"
+INGEST_OSM=true
+OSM_DL_LINK="http://download.geofabrik.de/europe/germany/nordrhein-westfalen-latest.osm.pbf"
 
 docker_tag="ec-mapmatching/mapserver"
 
@@ -13,8 +14,33 @@ mapserver_mode="slim"
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${scriptDir}" #&& mkdir data
 
+# parsing opts
+# u = mapserver username
+# p = mapserver password
+# s = mapserver database name
+# i = boolean value for data ingestion
+# d = osm.pbf download link
+while getopts u:p:s:i:d: option
+do 
+    case "${option}" in 
+    u)  mapserver_user=${OPTARG};;
+    p)  mapserver_pass=${OPTARG};;
+    s)  mapserver_dbname=${OPTARG};;
+    d)  OSM_DL_LINK=${OPTARG};;
+    i)  if [ ${OPTARG} = "true" ] || [ ${OPTARG} = "false" ]; then
+            INGEST_OSM=${OPTARG}
+        fi;;
+    esac
+done
+
+
 # build docker image
 docker build -t="ec-mapmatching/mapserver" .
+
+echo "Docker image has been successfully created";
+if [ $INGEST_OSM = "false" ]; then
+    return 0    
+
 
 # run the docker image
 docker run -d \
@@ -25,8 +51,17 @@ docker run -d \
 # wait a certain amount of time for the database to be initialized...
 sleep 15
 
-# execute the import stuff and commit the changes. 
+# execute the import stuff...
 docker exec -it mapserver /bin/bash --rcfile /root/.bashrc -ci \
-    "wget -O /data/osm-data.osm.pbf $osm_dl_link && /mnt/map/osm/import.sh /data/osm-data.osm.pbf $mapserver_dbname $mapserver_user $mapserver_pass /mnt/map/tools/road-types.json $mapserver_mode"
+    "wget -O /data/osm-data.osm.pbf $OSM_DL_LINK && /mnt/map/osm/import.sh /data/osm-data.osm.pbf $mapserver_dbname $mapserver_user $mapserver_pass /mnt/map/tools/road-types.json $mapserver_mode"
+
+# wait a certain amount of time, otherwise it gets stuck
+sleep 15
+
+#... and commit the changes. 
 docker commit mapserver ec-mapmatching/mapserver
+
+# wait a certain amount of time
+sleep 10
+
 docker stop mapserver && docker rm mapserver
