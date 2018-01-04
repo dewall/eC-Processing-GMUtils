f40646e46bf1384c9281c059994c958ec807743f
dewall <a.dewall@52north.org>
2018-01-04 12:23:26 +0100

    chunking for the input split

--- ingestor/src/main/java/org/envirocar/processing/ec4geomesa/ingestor/input/DownloadTracksRecordReader.java
+++ ingestor/src/main/java/org/envirocar/processing/ec4geomesa/ingestor/input/DownloadTracksRecordReader.java
@@ -1,16 +1,20 @@
 package org.envirocar.processing.ec4geomesa.ingestor.input;
 
+import com.google.common.collect.Lists;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 import com.squareup.okhttp.ResponseBody;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 /**
@@ -19,26 +23,31 @@ import org.apache.log4j.Logger;
  */
 public class DownloadTracksRecordReader extends RecordReader<LongWritable, Text> {
 
-    private static final Logger LOG = Logger.getLogger(DownloadTracksRecordReader.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(DownloadTracksRecordReader.class.getName());
+
+    static {
+        LOGGER.getParent().addAppender(new ConsoleAppender());
+        LOGGER.setLevel(Level.INFO);
+    }
 
     private final Text currentValue = new Text();
     private final OkHttpClient client = new OkHttpClient();
 
-    private boolean downloaded = false;
-    private TextInputSplit textInput;
     private Configuration config;
+    private boolean downloaded = false;
+    private String[] downloadUrls;
+    private AtomicInteger counter = new AtomicInteger();
 
     @Override
     public void initialize(InputSplit is, TaskAttemptContext tac) throws IOException, InterruptedException {
-        this.textInput = (TextInputSplit) is;
         this.config = tac.getConfiguration();
+        this.downloadUrls = ((TextInputSplit) is).getText().split(",");
     }
 
     @Override
     public boolean nextKeyValue() throws IOException, InterruptedException {
         if (!downloaded) {
-            String enviroCarURL = textInput.getText();
-            LOG.info("Trying to download track: " + enviroCarURL);
+            String enviroCarURL = downloadUrls[counter.getAndIncrement()];
 
             Request request = new Request.Builder()
                     .url(enviroCarURL)
@@ -50,8 +59,10 @@ public class DownloadTracksRecordReader extends RecordReader<LongWritable, Text>
             this.currentValue.clear();
             this.currentValue.set(body.string());
 
-            downloaded = true;
-            LOG.info("Track successfully downloaded");
+            if (downloadUrls.length == counter.get()) {
+                LOGGER.info("Tracks successfully downloaded");
+                downloaded = true;
+            }
             return true;
         }
         return false;
