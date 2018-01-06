5e41d13c2352766ae10e1a5610025d571a4b9d83
dewall <a.dewall@52north.org>
2018-01-06 22:56:21 +0100

    phenomenon enum added

---
+++ core/src/main/java/org/envirocar/processing/ec4geomesa/core/model/Phenomenon.java
@@ -0,0 +1,58 @@
+package org.envirocar.processing.ec4geomesa.core.model;
+
+/**
+ *
+ * @author dewall
+ */
+public enum Phenomenon {
+    CO2("CO2"),
+    CALCULATED_MAF("Calculated MAF"),
+    CONSUMPTION("Consumption"),
+    ENGINE_LOAD("Engine Load"),
+    FUEL_SYSTEM_LOOP("Fuel System Loop"),
+    FUEL_SYSTEM_STATUS_CODE("Fuel System Status Code"),
+    GPS_ACCURACY("GPS Accuracy"),
+    GPS_ALTITUDE("GPS Altitude"),
+    GPS_BEARING("GPS Bearing"),
+    GPS_HDOP("GPS HDOP"),
+    GPS_PDOP("GPS PDOP"),
+    GPS_VDOP("GPS VDOP"),
+    GPS_SPEED("GPS SPEED"),
+    INTAKE_PRESSURE("Intake Pressure"),
+    INTAKE_TEMPERATURE("Intake Temperature"),
+    LONG_TERM_FUEL_TRIM_1("Long-Term Fuel Trim 1"),
+    MAF("MAF"),
+    O2_LAMBDA_CURRENT("O2 Lambda Current"),
+    O2_LAMBDA_CURRENT_ER("O2 Lambda Current ER"),
+    O2_LAMBDA_VOLTAGE("O2 Lambda Voltage"),
+    O2_LAMBDA_VOLTAGE_ER("O2 Lambda Voltage ER"),
+    RPM("Rpm"),
+    SHORT_TERM_FUEL_TRIM_1("Short-Term Fuel Trim 1"),
+    SPEED("Speed"),
+    THROTTLE_POSITION("Throttle Position");
+
+    private final String readableName;
+
+    Phenomenon(String readableName) {
+        this.readableName = readableName;
+    }
+
+    public String getReadableName() {
+        return this.readableName;
+    }
+
+    @Override
+    public String toString() {
+        return readableName;
+    }
+
+    public static Phenomenon getByReadableName(String toFind) {
+        for (Phenomenon p : values()) {
+            if (p.toString().equalsIgnoreCase(toFind)) {
+                return p;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+}
